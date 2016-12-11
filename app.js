fb73a5388d5b799a7340c7b8025a20d13f64f3e5
Ward Debaene <ward.debaene.t4178@student.hogent.be>
2016-12-11 15:05:41 +0100

    niet meer in scope posts opslaan maar in factory

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-var app = angular.module('flapperNews', []);
-
-app.controller('MainCtrl', [
-'$scope',
-function($scope){
-  $scope.posts = [
+var app = angular.module('flapperNews', ['ui.router']);
+app.factory('posts', [function(){
+var o = {
+    posts: [
   {title: 'post 1', upvotes: 5},
   {title: 'post 2', upvotes: 2},
   {title: 'post 3', upvotes: 15},
   {title: 'post 4', upvotes: 9},
   {title: 'post 5', upvotes: 4}
-];
+]
+  };
+  return o;
+}]);
+
+app.controller('MainCtrl', [
+'$scope',
+'posts',
+function($scope, posts){
+  $scope.posts = posts.posts;
 $scope.addPost = function(){
   if(!$scope.title || $scope.title === '') { return; }
   $scope.posts.push({
