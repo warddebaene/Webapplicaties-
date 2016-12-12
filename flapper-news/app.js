b09845e7638103c7a89cc942f61d453e83f83dc6
Ward Debaene <ward.debaene.t4178@student.hogent.be>
2016-12-12 13:02:43 +0100

    Passport toevoegen aan de applicatie

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -5,11 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var passport = require('passport');
 
 mongoose.connect('mongodb://localhost/news');
 
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -27,6 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', index);
 app.use('/users', users);
